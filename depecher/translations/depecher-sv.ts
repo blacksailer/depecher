a7cc15f7da280e5704b06f69e7f6b9bd630fadaf
Åke Engelbrektson <eson57@users.noreply.github.com>
2018-04-27 07:38:26 +0200

    Create depecher-sv.ts
    Hi,
    Here,s a Swedish translation if you want. 
    I also attach some screens to show where translations don't work, or strings are missing.

---
+++ depecher/translations/depecher-sv.ts
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="Поделиться">
+        <source>Share link</source>
+        <extracomment>List header for link sharing method list</extracomment>
+        <translation>Dela länk</translation>
+    </message>
+    <message id="sailfish_browser-la-no_accounts">
+        <source>No sharing accounts available. You can add accounts in settings</source>
+        <extracomment>Empty state for share link page</extracomment>
+        <translation>Inga delningskonton tillgängliga. Du kan lägga till konton i Inställningar.</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation>Donationer</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>Källkod</translation>
+    </message>
+</context>
+<context>
+    <name>AreYouSureDialog</name>
+    <message>
+        <source>Is phone correct?</source>
+        <translation>Är det rätt telefon?</translation>
+    </message>
+    <message>
+        <source>Connection status: </source>
+        <translation>Anslutningsstatus: </translation>
+    </message>
+    <message>
+        <source>To continue connection to Telegram is needed</source>
+        <translation>Anslutning till Telegram krävs, för att fortsätta.</translation>
+    </message>
+</context>
+<context>
+    <name>AttachComponent</name>
+    <message>
+        <source>Фото</source>
+        <translation>Bilder</translation>
+    </message>
+    <message>
+        <source>Отменить</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Прикрепить</source>
+        <translation>Bifoga</translation>
+    </message>
+</context>
+<context>
+    <name>AuthorizeDialog</name>
+    <message>
+        <source>Insert your phone number and press &apos;Next&apos;</source>
+        <translation>Ange ditt telefonnummer och tryck &quot;Nästa&quot;</translation>
+    </message>
+    <message>
+        <source>Country Code</source>
+        <translation>Landskod</translation>
+    </message>
+    <message>
+        <source>Phone Number</source>
+        <translation>Telefonnummer</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Welcome</source>
+        <translation>Välkommen</translation>
+    </message>
+    <message>
+        <source>Attention: If Telegram is blocked in your country, first setup
+proxy server in settings via PulleyMenu</source>
+        <translation>OBS! Om Telegram blockeras i ditt land, måste du först konfigurera en proxyserver i inställningarna, via toppmenyn.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Depecher</source>
+        <translation>Depecher</translation>
+    </message>
+    <message>
+        <source>Unread 
+ messages</source>
+        <translation> Olästa 
+ meddelanden</translation>
+    </message>
+</context>
+<context>
+    <name>DialogsPage</name>
+    <message>
+        <source>Menu</source>
+        <translation>Meny</translation>
+    </message>
+    <message>
+        <source>Reset dialogs</source>
+        <translation>Återställ dialoger</translation>
+    </message>
+</context>
+<context>
+    <name>MenuComponent</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+</context>
+<context>
+    <name>MessagingPage</name>
+    <message>
+        <source>Copy Text</source>
+        <translation>Kopiera text</translation>
+    </message>
+    <message>
+        <source>new messages</source>
+        <translation>Nya meddelanden</translation>
+    </message>
+    <message>
+        <source>get newer</source>
+        <translation>Hämta nya</translation>
+    </message>
+</context>
+<context>
+    <name>PicturePage</name>
+    <message>
+        <source>Save to gallery</source>
+        <translation>Spara i galleriet</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Socks5 proxy</source>
+        <translation>Socks5 proxy</translation>
+    </message>
+    <message>
+        <source>Server address</source>
+        <translation>Serveradress</translation>
+    </message>
+    <message>
+        <source>Server port</source>
+        <translation>Serverport</translation>
+    </message>
+    <message>
+        <source>port</source>
+        <translation>Port</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Användarnamn</translation>
+    </message>
+    <message>
+        <source>username</source>
+        <translation>Användarnamn</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Lösenord</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Allmänt</translation>
+    </message>
+    <message>
+        <source>Log out</source>
+        <translation>Logga ut</translation>
+    </message>
+    <message>
+        <source>Proxy is ready</source>
+        <translation>Proxyn är redo</translation>
+    </message>
+    <message>
+        <source>Set proxy</source>
+        <translation>Tillämpa</translation>
+    </message>
+    <message>
+        <source>Address. Set empty to disable proxy</source>
+        <translation>Adress. Lämnas tom för att inaktivera proxy</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>SignInDialog</name>
+    <message>
+        <source>Code</source>
+        <translation>Kod</translation>
+    </message>
+    <message>
+        <source>First Name</source>
+        <translation>Förnamn</translation>
+    </message>
+    <message>
+        <source>Surname</source>
+        <translation>Efternamn</translation>
+    </message>
+    <message>
+        <source>Sign In</source>
+        <translation>Logga in</translation>
+    </message>
+    <message>
+        <source>Sign up</source>
+        <translation>Skapa ett konto</translation>
+    </message>
+    <message>
+        <source>Sending request...</source>
+        <translation>Skickar begäran...</translation>
+    </message>
+</context>
+</TS>
