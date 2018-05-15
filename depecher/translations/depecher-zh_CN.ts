e7fe62a3773240cef59f53b8ea8cfcdf8a3a628c
BirdZhang <0312birdzhang@gmail.com>
2018-05-15 18:51:44 +0800

    Add Simple Chinese  language translation

---
+++ depecher/translations/depecher-zh_CN.ts
@@ -0,0 +1,427 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name></name>
+    <message id="sailfish_browser-la-no_accounts">
+        <source>No sharing accounts available. You can add accounts in settings</source>
+        <extracomment>Empty state for share link page</extracomment>
+        <translation>没有可用的共享帐户, 您可以在设置中添加帐户</translation>
+    </message>
+    <message id="Share">
+        <source>Share link</source>
+        <extracomment>List header for link sharing method list</extracomment>
+        <translation>分享链接</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>源代码</translation>
+    </message>
+    <message>
+        <source>Groups in Telegram</source>
+        <translation>在电报中的群组</translation>
+    </message>
+    <message>
+        <source>English speaking community</source>
+        <translation>讲英语的社区</translation>
+    </message>
+    <message>
+        <source>Error %1</source>
+        <translation>错误 %1</translation>
+    </message>
+    <message>
+        <source>Joining chat</source>
+        <translation>加入讨论中</translation>
+    </message>
+    <message>
+        <source>Be aware, after clicking you will join chat. Leaving chats not supported for now.</source>
+        <translation>请注意，点击后你会加入讨论组， 暂时不支持离开讨论组。</translation>
+    </message>
+    <message>
+        <source>Depecher - Another Telegram client for Sailfish OS based on tdlib.
+
+Features:
+- Send/View/Delete messages
+- View photos
+- View stickers
+- Uploading/Downloading photos/docs
+- Receive notifications
+- 2FA authorization enabled
+
+No daemon yet.
+
+
+Thanks to:
+- @kaffeine
+- @chuvilin
+- @icoderus
+- @aa13q
+</source>
+        <translation>Depecher - 另一个基于tdlib的旗鱼电报客户端.
+
+功能:
+- 发送/查看/删除 消息
+- 查看图片
+- 查看贴纸
+- 上传/下载 图片/文档
+- 接收通知
+- 两步认证
+
+暂无后台进程。
+
+
+感谢:
+- @kaffeine
+- @chuvilin
+- @icoderus
+- @aa13q
+</translation>
+    </message>
+    <message>
+        <source>Here enumerated groups where you can ask any questions (general,dev etc.) related to Sailfish OS.
+This groups lives only because of community and by community around Sailfish OS.</source>
+        <translation>在这里列举的组可以提出与Sailfish操作系统相关的任何问题（一般，开发等）。
+这些小组正是因为交流和与Sailfish操作系统有关的社区而存在。</translation>
+    </message>
+</context>
+<context>
+    <name>AreYouSureDialog</name>
+    <message>
+        <source>Is phone correct?</source>
+        <translation>手机号是否正确？</translation>
+    </message>
+    <message>
+        <source>Connection status: </source>
+        <translation>连接状态：</translation>
+    </message>
+    <message>
+        <source>To continue connection to Telegram is needed</source>
+        <translation>要继续首先要连接到电报</translation>
+    </message>
+</context>
+<context>
+    <name>AttachComponent</name>
+    <message>
+        <source>Photo</source>
+        <translation>照片</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>发送</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <source>Document</source>
+        <translation>文档</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>主页</translation>
+    </message>
+    <message>
+        <source>Go to Home folder</source>
+        <translation>回到主文件夹</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>文档</translation>
+    </message>
+    <message>
+        <source>Send a document</source>
+        <translation>发送文档</translation>
+    </message>
+    <message>
+        <source>Files to send</source>
+        <translation>要发送的文件</translation>
+    </message>
+</context>
+<context>
+    <name>AuthorizeDialog</name>
+    <message>
+        <source>Insert your phone number and press &apos;Next&apos;</source>
+        <translation>输入电话号码并按&apos;下一步&apos;</translation>
+    </message>
+    <message>
+        <source>Country Code</source>
+        <translation>国家代码</translation>
+    </message>
+    <message>
+        <source>Phone Number</source>
+        <translation>手机号码</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Welcome</source>
+        <translation>欢迎</translation>
+    </message>
+    <message>
+        <source>Attention: If Telegram is blocked in your country, first setup
+proxy server in settings via PulleyMenu</source>
+        <translation>注意：如果电报在你的国家被禁用，请先通过下拉菜单
+设置代理</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Depecher</source>
+        <translation>Depecher</translation>
+    </message>
+    <message>
+        <source>Unread 
+ messages</source>
+        <translation>未读消息</translation>
+    </message>
+</context>
+<context>
+    <name>DialogsPage</name>
+    <message>
+        <source>Reset dialogs</source>
+        <translation>重置对话</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>MenuComponent</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>MessageItem</name>
+    <message>
+        <source>new messages</source>
+        <translation>新消息</translation>
+    </message>
+</context>
+<context>
+    <name>MessagingPage</name>
+    <message>
+        <source>Copy Text</source>
+        <translation>复制文本</translation>
+    </message>
+    <message>
+        <source>get newer</source>
+        <translation>获取更新</translation>
+    </message>
+    <message>
+        <source>Delete Message</source>
+        <translation>上传消息</translation>
+    </message>
+    <message>
+        <source>Deleting...</source>
+        <translation>删除中...</translation>
+    </message>
+    <message>
+        <source>Delete for everyone</source>
+        <translation>删除所有人</translation>
+    </message>
+</context>
+<context>
+    <name>PicturePage</name>
+    <message>
+        <source>Save to gallery</source>
+        <translation>保存到图库</translation>
+    </message>
+    <message>
+        <source>Image saved to gallery!</source>
+        <translation>图片已保存到图库！</translation>
+    </message>
+    <message>
+        <source>Share function will be here soon</source>
+        <translation>可用的分享方法将在此显示</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Socks5 proxy</source>
+        <translation>Socks5代理</translation>
+    </message>
+    <message>
+        <source>Server address</source>
+        <translation>服务器地址</translation>
+    </message>
+    <message>
+        <source>Server port</source>
+        <translation>服务器端口</translation>
+    </message>
+    <message>
+        <source>port</source>
+        <translation>端口</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>用户名</translation>
+    </message>
+    <message>
+        <source>username</source>
+        <translation>用户名</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>通用</translation>
+    </message>
+    <message>
+        <source>Log out</source>
+        <translation>登出</translation>
+    </message>
+    <message>
+        <source>Proxy is ready</source>
+        <translation>代理可用</translation>
+    </message>
+    <message>
+        <source>Set proxy</source>
+        <translation>设置代理</translation>
+    </message>
+    <message>
+        <source>Address. Set empty to disable proxy</source>
+        <translation>地址，设置为空则禁用代理</translation>
+    </message>
+    <message>
+        <source>Connection status: </source>
+        <translation>连接状态：</translation>
+    </message>
+    <message>
+        <source>About program</source>
+        <translation>关于程序</translation>
+    </message>
+    <message>
+        <source>Credits and stuff</source>
+        <translation>学分之类的</translation>
+    </message>
+</context>
+<context>
+    <name>SignInDialog</name>
+    <message>
+        <source>Code</source>
+        <translation>代码</translation>
+    </message>
+    <message>
+        <source>First Name</source>
+        <translation>名字</translation>
+    </message>
+    <message>
+        <source>Surname</source>
+        <translation>姓</translation>
+    </message>
+    <message>
+        <source>Sign In</source>
+        <translation>登录</translation>
+    </message>
+    <message>
+        <source>Sign up</source>
+        <translation>注册</translation>
+    </message>
+    <message>
+        <source>Sending request...</source>
+        <translation>发送请求中...</translation>
+    </message>
+    <message>
+        <source>Enter your password</source>
+        <translation>输入你的密码</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <source>Recover password</source>
+        <translation>恢复密码</translation>
+    </message>
+    <message>
+        <source>Email is sent</source>
+        <translation>邮件已发送</translation>
+    </message>
+    <message>
+        <source>Show hint</source>
+        <translation>显示提示</translation>
+    </message>
+    <message>
+        <source>Enter recovery code. 
+ Remember! After that 2FA authorization will be disabled</source>
+        <translation>输入恢复码。
+记住！这样会禁用两步验证</translation>
+    </message>
+    <message>
+        <source>Recovery code</source>
+        <translation>恢复码</translation>
+    </message>
+    <message>
+        <source>Wait for the message via %1 containing the activation code and press %2</source>
+        <translation>等待包含激活码的%1消息，然后按%2</translation>
+    </message>
+    <message>
+        <source>recovery code sent to %1</source>
+        <translation>恢复码发送到%1</translation>
+    </message>
+    <message>
+        <source>Password hint</source>
+        <translation>密码提示</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>Waiting for network</source>
+        <translation>等待网络中</translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation>连接中</translation>
+    </message>
+    <message>
+        <source>Connecting to proxy</source>
+        <translation>连接到代理中</translation>
+    </message>
+    <message>
+        <source>Updating...</source>
+        <translation>更新中...</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>可用</translation>
+    </message>
+    <message>
+        <source>Uknown</source>
+        <translation>未知</translation>
+    </message>
+</context>
+</TS>
